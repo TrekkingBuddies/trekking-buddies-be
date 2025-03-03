fa80ef8b018ee83e83aa1ef52f8296aa38808609
Oleksandra Hryshchenko <galexa0320@gmail.com>
2025-03-03 19:56:22 +0000

    user routes + authentication is done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,14 @@
-const createUser = require("../models/users.model");
+const {
+  createUser,
+  selectUserById,
+  updateUserById,
+  removeUserById,
+  selectUsers,
+} = require("../models/users.model");
 
 exports.postUser = (req, res, next) => {
-  createUser(req.params.userId, req.body)
+  const uid = req.user.uid; // from verifyUser
+  createUser(uid, req.body)
     .then((user) => {
       res.status(201).send({ user: user.data() });
     })
@@ -13,7 +20,37 @@ exports.postUser = (req, res, next) => {
 exports.getUserById = (req, res, next) => {
   selectUserById(req.params.userId)
     .then((user) => {
-      res.status(200).send({ user: user.data() });
+      res.status(200).send({ user: user });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchUser = (req, res, next) => {
+  updateUserById(req.params.userId, req.body)
+    .then((user) => {
+      res.status(200).send({ user: user });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.deleteUser = (req, res, next) => {
+  removeUserById(req.params.userId)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getUsers = (req, res, next) => {
+  selectUsers()
+    .then((users) => {
+      res.status(200).send({ users: users });
     })
     .catch((err) => {
       next(err);
